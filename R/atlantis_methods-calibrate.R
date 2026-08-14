3cd9f96e858769eae5be67fb7433ac99c15c82e0
Kevin Cazelles <kevin.cazelles@insileco.io>
2026-08-14 12:11:23 -0400

    Remove atlantis_run_model

--- R/atlantis_methods-calibrate.R
+++ R/atlantis_methods-calibrate.R
@@ -133,34 +133,6 @@ format_calibration_entry <- function(x, prm, version = "3-6722") {
 }
 
 
-#' Run an Atlantis model for calibration
-#'
-#' Run one calibration iteration: validate the calibration list, apply the
-#' parameter transformations, write the input files and run script, run the
-#' model and compute the objective function.
-#'
-#' @param par Numeric vector of parameter values to calibrate.
-#' @param atlantis An `Atlantis` object.
-#' @param version Atlantis version identifier. Default is `"3-6722"`.
-#'
-#' @noRd
-atlantis_run_model <- function(
-  par,
-  atlantis,
-  version = "3-6722"
-) {
-  cli::cli_progress_step("Validating callibration list")
-
-  cli::cli_progress_step("Applying tranformation")
-
-  cli::cli_progress_step("Writing new input files and script")
-
-  cli::cli_progress_step("Running the model")
-
-  cli::cli_progress_step("Computing the objective function")
-}
-
-
 pow10 <- function(x) {
   exp(log(10) * x)
 }
